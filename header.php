c293fef1cd4f4562cabec30cf10b2a876ef0a8d5
大西友香 <oonishiyuka@oonishiyukanoMacBook-ea.local>
2020-03-24 18:57:51 +0900

    準備

--- header.php
+++ header.php
@@ -3,54 +3,80 @@ $home = esc_url(home_url());
 $wp_url = get_template_directory_uri(); ?>
 <!DOCTYPE HTML>
 <html lang="ja">
+
 <head>
-<meta charset="UTF-8">
+<meta charset="utf-8">
 <meta name="viewport" content="width=device-width,initial-scale=1.0">
+<link rel="stylesheet" href="<?php echo $wp_url ?>/lib/css/jquery.fatNav.min.css">
+<link rel="stylesheet" href="<?php echo $wp_url ?>/lib/css/slick.css"/>
+<link rel="stylesheet" href="<?php echo $wp_url ?>/lib/css/slick-theme.css"/>
+<link rel="stylesheet" href="<?php echo $wp_url ?>/lib/css/header.css">
+<link rel="stylesheet" href="<?php echo $wp_url ?>/lib/css/footer.css">
+<link rel="stylesheet" href="<?php echo $wp_url ?>/lib/css/common.css">
+<link rel="stylesheet" href="<?php echo $wp_url ?>/lib/css/sub-mv.css">
+<link rel="stylesheet" href="<?php echo $wp_url ?>/lib/css/top.css">
+<link rel="stylesheet" href="<?php echo $wp_url ?>/lib/css/concept.css">
+<link rel="stylesheet" href="<?php echo $wp_url ?>/lib/css/plan.css">
+<link rel="stylesheet" href="<?php echo $wp_url ?>/lib/css/flow.css">
 <?php wp_head(); ?>
 <?php if (!is_user_logged_in()): ?>
 <!-- ここにGAトラッキングタグ -->
 <?php endif; ?>
 </head>
+
 <body>
 
 <!-- ヘッダー -->
-<header id="header">
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <div class="container">
-      <a class="navbar-brand" href="<?php echo $home; ?>/">Navbar</a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav mr-auto">
-          <li class="nav-item active">
-            <a class="nav-link" href="<?php echo $home; ?>/">Home <span class="sr-only">(current)</span></a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
-          </li>
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
-            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-              <a class="dropdown-item" href="#">Action</a>
-              <a class="dropdown-item" href="#">Another action</a>
-              <div class="dropdown-divider"></div>
-              <a class="dropdown-item" href="#">Something else here</a>
-            </div>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-          </li>
-        </ul>
-        <form class="form-inline my-2 my-lg-0">
-          <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-        </form>
-      </div>
-    </div>
-  </nav>
+<header>
+  <div class="img-wrap">
+    <img src="<?php echo $wp_url ?>/lib/images/common/logo.png" alt="タイトルがはいります">
+  </div>
+  <div class="fix-btn">
+    <a href="https://kyoshin-korekara.etudes.jp/jp/top/dashboard" target="_blank"><img src="<?php echo $wp_url ?>/lib/images/header/fix_btn.png" alt="今すぐ受講する"></a>
+  </div>
 </header>
 <!-- ヘッダー終了 -->
 
 <!-- メインコンテンツ -->
 <main>
+  <?php if (is_home() || is_front_page()): ?>
+  <div id="mv">
+  </div>
+  <!-- スマホヘッダー -->
+  <div id="sp-header">
+    <div class="fat-nav">
+      <div class="fat-nav__wrapper">
+        <nav class="gnav">
+          <div class="gnav__wrap">
+            <ul class="gnav__menu">
+              <li><a href="<?php echo $home ?>/consept">consept<span>コンセプト</span></a></li>
+              <li><a href="<?php echo $home ?>/plan">plan<span>プラン</span></a></li>
+              <li><a href="<?php echo $home ?>/flow">flow<span>体験の流れ</span></a></li>
+              <li><a href="<?php echo $home ?>/gallery">gallery<span>フォトギャラリー</span></a></li>
+              <li><a href="<?php echo $home ?>/faq">faq<span>よくあるご質問</span></a></li>
+              <li><a href="<?php echo $home ?>/blog">blog<span>ブログ</span></a></li>
+            </ul>
+          </div>
+        </nav>
+      </div>
+    </div>
+  </div>
+  <!-- スマホヘッダー終了 -->
+
+  <!-- PCヘッダー -->
+  <nav id="pc-header" class="flex pc-only">
+    <div class="content">
+      <ul>
+        <li><a href="<?php echo $home ?>/consept">consept<span>コンセプト</span></a></li>
+        <li><a href="<?php echo $home ?>/plan">plan<span>プラン</span></a></li>
+        <li><a href="<?php echo $home ?>/flow">flow<span>体験の流れ</span></a></li>
+        <li><a href="<?php echo $home ?>/gallery">gallery<span>フォトギャラリー</span></a></li>
+        <li><a href="<?php echo $home ?>/faq">faq<span>よくあるご質問</span></a></li>
+        <li><a href="<?php echo $home ?>/blog">blog<span>ブログ</span></a></li>
+      </ul>
+    </div>
+  </nav>
+  <!-- PCヘッダー終了 -->
+<?php else: ?>
+
+<?php endif; ?>
