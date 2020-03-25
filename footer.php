06ca138ef3ac87ec075c91c7c3a326098e5ed3b7
大西友香 <oonishiyuka@oonishiyukanoMacBook-ea.local>
2020-03-25 13:38:12 +0900

    mv

--- footer.php
+++ footer.php
@@ -5,12 +5,55 @@ $wp_url = get_template_directory_uri(); ?>
 <!-- メインコンテンツ終了 -->
 
 <!-- フッター -->
-<footer class="py-4 bg-dark text-white">
-  <div class="container">
-    <small class="d-block">©2019 .inc</small>
+<footer id="footer">
+  <div class="content-01 flex">
+    <div class="left">
+      <div class="img-wrap">
+        <img src="<?php echo $wp_url ?>/lib/images/common/logo.png" alt="<?php bloginfo( 'name' ); ?>">
+      </div>
+    </div>
+    <div class="right">
+      <div class="">
+        <p>〇〇店(本店)</p>
+        <div class="flex">
+          <div class="right-l">
+            <p>〒000-0000 京都市下京区東塩小路町736番地3階<a href="#">map</a></p>
+            <p><span>TEL</span>000-0000-0000</p>
+            <p><span>営業時間</span>10:00~18:00 ※月曜定休</p>
+          </div>
+          <div class="right-r">
+            <ul>
+              <li><a href="<?php echo $home ?>/consept">コンセプト</a></li>
+              <li><a href="<?php echo $home ?>/plan">プラン</a></li>
+              <li><a href="<?php echo $home ?>/flow">体験の流れ</a></li>
+              <li><a href="<?php echo $home ?>/gallery">フォトギャラリー</a></li>
+              <li><a href="<?php echo $home ?>/faq">よくあるご質問</a></li>
+              <li><a href="<?php echo $home ?>/blog">ブログ</a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="content-02">
+    <ul>
+      <li><a href="<?php echo $home ?>/privacy policy">プライバシーポリシー</a></li>
+      <li><a href="<?php echo $home ?>/sitemap">サイトマップ</a></li>
+      <li><a href="<?php echo $home ?>/flow">permalink</a></li>
+    </ul>
   </div>
 </footer>
 <!-- フッター終了 -->
+
+<!-- js -->
+<script src="<?php echo $wp_url ?>/lib/js/jquery.min.js"></script>
+<script src="<?php echo $wp_url; ?>/lib/js/jquery.fatNav.min.js"></script>
+<script src="<?php echo $wp_url ?>/lib/js/jquery.bgswitcher.js"></script>
+<script src="<?php echo $wp_url ?>/lib/js/slick.min.js"></script>
+<script src="<?php echo $wp_url ?>/lib/js/vegas.min.js"></script>
+<script src="<?php echo $wp_url ?>/lib/js/app.js"></script>
+<!-- js終了 -->
+
 <?php wp_footer(); ?>
 </body>
 </html>
--- header.php
+++ header.php
@@ -7,12 +7,15 @@ $wp_url = get_template_directory_uri(); ?>
 <head>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width,initial-scale=1.0">
+<link href="https://fonts.googleapis.com/css?family=Libre+Baskerville|Noto+Sans+JP:400,700|Noto+Serif+JP:400,700&display=swap" rel="stylesheet">
 <link rel="stylesheet" href="<?php echo $wp_url ?>/lib/css/jquery.fatNav.min.css">
 <link rel="stylesheet" href="<?php echo $wp_url ?>/lib/css/slick.css"/>
 <link rel="stylesheet" href="<?php echo $wp_url ?>/lib/css/slick-theme.css"/>
+<link rel="stylesheet" href="<?php echo $wp_url ?>/lib/css/vegas.min.css">
+<link rel="stylesheet" href="<?php echo $wp_url ?>/lib/css/common.css">
 <link rel="stylesheet" href="<?php echo $wp_url ?>/lib/css/header.css">
 <link rel="stylesheet" href="<?php echo $wp_url ?>/lib/css/footer.css">
-<link rel="stylesheet" href="<?php echo $wp_url ?>/lib/css/common.css">
+<link rel="stylesheet" href="<?php echo $wp_url ?>/lib/css/mv.css">
 <link rel="stylesheet" href="<?php echo $wp_url ?>/lib/css/sub-mv.css">
 <link rel="stylesheet" href="<?php echo $wp_url ?>/lib/css/top.css">
 <link rel="stylesheet" href="<?php echo $wp_url ?>/lib/css/concept.css">
@@ -26,27 +29,63 @@ $wp_url = get_template_directory_uri(); ?>
 
 <body>
 
-<!-- ヘッダー -->
-<header>
-  <div class="img-wrap">
-    <img src="<?php echo $wp_url ?>/lib/images/common/logo.png" alt="タイトルがはいります">
-  </div>
-  <div class="fix-btn">
-    <a href="https://kyoshin-korekara.etudes.jp/jp/top/dashboard" target="_blank"><img src="<?php echo $wp_url ?>/lib/images/header/fix_btn.png" alt="今すぐ受講する"></a>
-  </div>
-</header>
-<!-- ヘッダー終了 -->
+<?php
+global $wp_query;
+$post_obj = $wp_query->get_queried_object();
+$slug = $post_obj->post_name;
+?>
 
-<!-- メインコンテンツ -->
-<main>
-  <?php if (is_home() || is_front_page()): ?>
-  <div id="mv">
+<?php if (is_home() || is_front_page()): ?>
+
+<!-- トップページメインビジュアル -->
+<section id="mv" class="flex">
+  <div class="left">
+    <div class="content">
+      <div class="img-wrap">
+        <img class="logo" src="<?php echo $wp_url ?>/lib/images/common/logo_01.png" alt="<?php bloginfo( 'name' ); ?>">
+      </div>
+      <div class="copy">
+        <p class="serif">キ<br>ャ<br>ッ<br>チ<br>コ<br>ピ<br>ー<br>が<br>入<br>り<br>ま<br>す<br>。</p>
+        <p class="serif">キ<br>ャ<br>ッ<br>チ<br>コ<br>ピ<br>ー<br>が<br>入<br>り<br>ま<br>す<br>。</p>
+      </div>
+      <div class="tel-btn">
+        <a href="tel:000-0000-0000"><img src="<?php echo $wp_url ?>/lib/images/common/tel_01.png" alt="甲冑体験かすみの電話番号"></a>
+      </div>
+    </div>
+    <div class="booking-btn">
+      <a class="serif" href="#">空き状況を確認する</a>
+    </div>
   </div>
-  <!-- スマホヘッダー -->
-  <div id="sp-header">
+  <div class="bg-slider"></div>
+</section>
+<!-- トップページメインビジュアル終了 -->
+
+<?php else: ?>
+
+<!-- 下層ページメインビジュアル -->
+<section id="submv">
+    <div class="">
+      <p><?php echo $slug; ?></p>
+      <h2 class=""><?php echo get_the_title(); ?></h2>
+    </div>
+    <div class="img-wrap">
+      <img class="logo" src="<?php echo $wp_url ?>/lib/images/common/logo_02.png" alt="<?php bloginfo( 'name' ); ?>">
+    </div>
+    <div class="img-wrap">
+      <a href="tel:000-0000-0000"><img src="<?php echo $wp_url ?>/lib/images/common/tel_02.png" alt="甲冑体験かすみの電話番号"></a>
+    </div>
+</section>
+<!-- 下層ページメインビジュアル終了 -->
+
+<?php endif; ?>
+
+<main>
+<!-- メインコンテンツ -->
+<header>
+    <!-- スマホヘッダー -->
     <div class="fat-nav">
       <div class="fat-nav__wrapper">
-        <nav class="gnav">
+        <nav id="sp-header" class="gnav">
           <div class="gnav__wrap">
             <ul class="gnav__menu">
               <li><a href="<?php echo $home ?>/consept">consept<span>コンセプト</span></a></li>
@@ -60,23 +99,22 @@ $wp_url = get_template_directory_uri(); ?>
         </nav>
       </div>
     </div>
-  </div>
-  <!-- スマホヘッダー終了 -->
-
-  <!-- PCヘッダー -->
-  <nav id="pc-header" class="flex pc-only">
-    <div class="content">
-      <ul>
-        <li><a href="<?php echo $home ?>/consept">consept<span>コンセプト</span></a></li>
-        <li><a href="<?php echo $home ?>/plan">plan<span>プラン</span></a></li>
-        <li><a href="<?php echo $home ?>/flow">flow<span>体験の流れ</span></a></li>
-        <li><a href="<?php echo $home ?>/gallery">gallery<span>フォトギャラリー</span></a></li>
-        <li><a href="<?php echo $home ?>/faq">faq<span>よくあるご質問</span></a></li>
-        <li><a href="<?php echo $home ?>/blog">blog<span>ブログ</span></a></li>
-      </ul>
+    <!-- スマホヘッダー終了 -->
+    <!-- PCヘッダー -->
+    <nav id="pc-header" class="flex pc-only">
+      <div class="content">
+        <ul>
+          <li><a href="<?php echo $home ?>/consept">consept<span>コンセプト</span></a></li>
+          <li><a href="<?php echo $home ?>/plan">plan<span>プラン</span></a></li>
+          <li><a href="<?php echo $home ?>/flow">flow<span>体験の流れ</span></a></li>
+          <li><a href="<?php echo $home ?>/gallery">gallery<span>フォトギャラリー</span></a></li>
+          <li><a href="<?php echo $home ?>/faq">faq<span>よくあるご質問</span></a></li>
+          <li><a href="<?php echo $home ?>/blog">blog<span>ブログ</span></a></li>
+        </ul>
+      </div>
+    </nav>
+    <!-- PCヘッダー終了 -->
+    <div class="fix-btn">
+      <a href="#"><img src="<?php echo $wp_url ?>/lib/images/common/booking_btn.png" alt="空き情報を確認"></a>
     </div>
-  </nav>
-  <!-- PCヘッダー終了 -->
-<?php else: ?>
-
-<?php endif; ?>
+</header>
